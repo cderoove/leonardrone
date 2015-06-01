6eec735554f5e9589a9ef4fce6297e599d44b7ce
Kerem Eryilmaz <keryilmaz@gmail.com>
2015-06-01 13:15:58 +0300

    Added leapcontrol to project file and core.

--- leonardrone/project.clj
+++ leonardrone/project.clj
@@ -3,9 +3,12 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :main leonardrone.core
   :plugins [[lein-localrepo "0.5.3"]]
   :resource-paths ["leap_lib/LeapJava.jar" "resources"]  
   :jvm-opts  [~(str "-Djava.library.path=leap_lib/:" (System/getenv "LD_LIBRARY_PATH"))]
   :dependencies [[org.clojure/clojure "1.6.0"]
                  ;[clj-drone "0.1.8"]
                  ])
+
+
--- leonardrone/src/leonardrone/core.clj
+++ leonardrone/src/leonardrone/core.clj
@@ -1,6 +1,16 @@
-(ns leonardrone.core)
+(ns leonardrone.core
+  (:require leonardrone.leapcontrol :as leapcontrol)
+  (:require clojure-leap.core :as leap))
 
 (defn foo
   "I don't do a whole lot."
   [x]
   (println x "Hello, World!"))
+
+(defn -main [& args]
+  (let [listener (leap/listener :frame #(process-frame (:controller %) (:frame %) (:screens %))
+                                :default #(println "Toggling" (:state %) "for listener:" (:listener %)))
+        [controller _] (leap/controller listener)]
+    (println "Press Enter to quit")
+    (read-line)
+    (leap/remove-listener! controller listener)))
