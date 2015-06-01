822aee022971d9d2c4b444f7e50dfd5f95ee7c2d
Coen De Roover <cderoove@vub.ac.be>
2015-06-01 11:51:24 +0200

    included clojure-leap

--- leonardrone/project.clj
+++ leonardrone/project.clj
@@ -4,5 +4,8 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :plugins [[lein-localrepo "0.5.3"]]
+  :resource-paths ["leap_lib/LeapJava.jar" "resources"]  
+  :jvm-opts  [~(str "-Djava.library.path=leap_lib/:" (System/getenv "LD_LIBRARY_PATH"))]
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [clj-drone "0.1.8"]])
+                 ;[clj-drone "0.1.8"]
+                 ])
